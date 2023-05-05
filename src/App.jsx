eeb2121020cf8e906e73846db434f65ef65cea4d
Sanchit poudel <50201318+Sanchitpoudel@users.noreply.github.com>
2023-05-05 13:17:04 +0545

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import TweetBody from "./components/tweetbody";
+import Footer from "./components/footer";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <TweetBody />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
